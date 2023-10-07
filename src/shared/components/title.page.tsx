5500dc087d9a0d30c2fab10f5afb8b55145d27e2
usha <usha.chaudhary@esewa.com.np>
2023-10-07 08:05:35 +0545

    update typo and title

--- src/shared/components/title.page.tsx
+++ src/shared/components/title.page.tsx
@@ -1,5 +1,13 @@
 const TitleComponent = (props) => {
-  return <div className="text-2xl text-gray-800">{props.title}</div>;
+  return (
+    <h1
+      role="title"
+      aria-label="e-commmerce sign in page"
+      className="text-2xl text-gray-800"
+    >
+      {props.title}
+    </h1>
+  );
 };
 
 export default TitleComponent;
